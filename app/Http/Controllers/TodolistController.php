1cad2a85ef6b65c2dfb1d889362b89aac13e4677
Muh Yusril Ananda <muhamadyusrila@gmail.com>
2025-09-02 22:28:30 +0700

    create todolist page

--- app/Http/Controllers/TodolistController.php
+++ app/Http/Controllers/TodolistController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\TodolistService;
 use Illuminate\Http\Request;
 
 class TodolistController extends Controller
 {
-    public function indexTodo(Request $request) {
+    private TodolistService $todolistService;
 
+    public function __construct(TodolistService $todolistService) {
+        $this->todolistService = $todolistService;
+    }
+
+    public function indexTodo(Request $request) {
+        $todolist = $this->todolistService->getTodoList();
+
+        return response()->view("todolist.index-todolist", [
+            "title" => "Todolist",
+            "todolist" => $todolist
+        ]);
     }
 
     public function addTodo(Request $request) {
